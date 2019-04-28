0b7496de72c985c9f2e8a8129a976dbbdde04b87
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2019-04-28 17:12:42 +0900

    Create scatter_plot2.py

---
+++ 04.데이터 시각화/scatter_plot2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 28 16:51:24 2019
+
+@author: loveAlakazam
+"""
+import random
+from matplotlib import pyplot as plt
+
+# n: the number of students
+n=5
+ 
+test_1_grades= [ random.randrange(0,100) for i in range(n)]
+test_2_grades= [ random.randrange(0,100) for i in range(n)]
+
+# x축: test_1_grades, y축: test_2_grades
+plt.scatter(test_1_grades, test_2_grades)
+plt.title("The Axes aren't comparable")
+
+plt.xlabel("test 1 grades")
+plt.ylabel("test 2 grades")
+plt.show()
