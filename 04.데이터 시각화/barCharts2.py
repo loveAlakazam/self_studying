96f38739ea4b539c821d0834e2132d5afd485692
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2019-04-28 14:41:11 +0900

    Add files via upload

---
+++ 04.데이터 시각화/barCharts2.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 28 03:04:28 2019
+
+@author: loveAlakazam
+"""
+from matplotlib import pyplot as plt
+import random
+from collections import Counter
+
+# 0~100사이의 수를 랜덤
+n=10 #people
+
+# n명의 사람들 점수표
+grades=[ random.randrange(0,101) for i in range(n+1)]
+print(grades)
+
+decile = lambda grade : grade //10*10
+histogram = Counter(decile(grade) for grade in grades)
+plt.bar([ x for x in histogram.keys()], #각막대를 왼쪽으로 4만큼 옮기고
+         histogram.values(), #각 막대의 높이를 정한다.
+         8) #너비 8
+plt.axis([-5, 105, 0, 5]) #x축은 -5부터 105, y축은 0부터 5
+
+plt.xticks([10*i for i in range(11)])
+plt.xlabel("Decile")
+plt.ylabel("# of Students")
+
+plt.title("Distribution of Exam 1 Grades")
+plt.show()
